b743da472301b169d5fab8168829fee884ee78cf
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-05-13 22:49:27 +0200

    syncho call + stout returned from server

--- app/client/main.js
+++ app/client/main.js
@@ -8,15 +8,17 @@ Template.hello.events({
 		var url = event.target.url.value;
 		var id=Random.id();
 
-		var cmd='cd /home/wpiotrowiak;';
-		cmd+='mkdir '+id+';';
-		cmd+='cd '+id+';';
-		cmd+='wget '+url+';';
-		cmd+='cd .. ;';
-		cmd+='zip '+id+' '+id+' ;';
+		var cmd='cd /home/wpiotrowiak && ';
+		cmd+='mkdir '+id+' && ';
+		cmd+='cd '+id+' && ';
+		cmd+='wget '+url+' && ';
+		cmd+='cd .. && ';
+		cmd+='zip '+id+' '+id+' && ';
 		cmd+='rm -Rf '+id;
 	
-		Meteor.call('action', cmd);
+		Meteor.call('action', url,function(error,result){
+		alert(result.stdout);
+		});
 		return false;
 	}
   });
--- app/server/main.js
+++ app/server/main.js
@@ -2,15 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.methods({
   	action: function (command) {
- 	const exec = require('child_process').exec;
-	const child = exec(command,
-	  (error, stdout, stderr) => {
- 	   console.log(`stdout: ${stdout}`);
-	    console.log(`stderr: ${stderr}`);
-	    if (error !== null) {
-	      console.log(`exec error: ${error}`);
-	    }
-	});
+ 	var exec = require('sync-exec');
+	return exec(command);
 	}
 });
 
