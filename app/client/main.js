882c8fdcecaa2141368efd1f59989d55b55c7416
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-05-14 21:38:54 +0200

    response passed into textarea. planned to have history of calls

--- app/client/main.js
+++ app/client/main.js
@@ -4,7 +4,23 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.hello.events({
+	"keydown input.url": function(event) {
+		if(event.which==38)
+		{
+		alert('up');
+		}
+	},
+
 	"submit .command-run": function (event) {
+		var command = event.target.url.value;
+		Meteor.call('action', command,function(error,result){
+		$('#console').val(result.stdout);
+		});
+		event.target.url.value="";
+		return false;
+	},
+
+	"submit .download": function (event) {
 		var url = event.target.url.value;
 		var id=Random.id();
 
@@ -16,8 +32,8 @@ Template.hello.events({
 		cmd+='zip '+id+' '+id+' && ';
 		cmd+='rm -Rf '+id;
 	
-		Meteor.call('action', url,function(error,result){
-		alert(result.stdout);
+		Meteor.call('action', cmd,function(error,result){
+		
 		});
 		return false;
 	}
