4deece0caf252fdd18685f0939358645fdadc1aa
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-05-18 22:24:52 +0200

    simple mongodb support

--- app/client/main.js
+++ app/client/main.js
@@ -2,6 +2,17 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+Calls = new Mongo.Collection('calls');
+
+Template.main.onCreated(function bodyOnCreated() {
+  Meteor.subscribe('calls');
+});
+
+Template.main.helpers({
+	calls: function () {
+		 return Calls.find({}).count();
+	 }
+ });
 
 Template.main.events({
 	"keydown input.url": function(event) {
@@ -9,8 +20,6 @@ Template.main.events({
 		{
 			var url=event.target.value;
 			var id=Random.id();
-
-
 			var createDir='mkdir '+id;
 
 			var downloadFile='cd '+id+' && wget '+url;
@@ -18,6 +27,9 @@ Template.main.events({
 			var prepareZip='zip '+id+' '+id+' && ';
 			prepareZip+='rm -Rf '+id;
 
+
+			Meteor.call('registerCall', url,id,function(error,result){
+			});
 
 			Meteor.call('action', createDir,function(error,result){
 			$('#console').val($('#console').val()+result.stdout);
--- app/server/main.js
+++ app/server/main.js
@@ -1,8 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 
+Calls = new Mongo.Collection('calls');
+
+
+Meteor.publish('calls', function tasksPublication() {
+  return Calls.find();
+});
+
 Meteor.methods({
   	action: function (command) {
  	var exec = require('sync-exec');
 	return exec(command,{cwd: '/home/wpiotrowiak/wraps'});
-	}
+},
+
+registerCall: function (url,id) {
+Calls.insert({
+  url,id,
+  createdAt: new Date()
+});
+}
 });
